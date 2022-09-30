2402d4fab40533fee21d98a545fdf1963aac0835
Sahil Nigam <153sahilnigam@gmail.com>
2022-09-30 07:24:21 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// var svg1 = document.querySelectorAll(".svg1")
+// let vis = false
+// setInterval( function () {
+//     if(vis){
+//         svg1[0].style.fill = "rgba(128, 0, 128, 0.5)"
+//         svg1[1].style.fill = "rgba(128, 0, 128, 0.5)"
+//         vis = false
+//     }
+//     else{
+//         svg1[0].style.fill = "#00FFC2"
+//         svg1[1].style.fill = "#00FFC2"
+//         vis = true
+//     }
+// }, 2000)
+
+setTimeout(function(){
+    window.location.hash = "row12"
+}, 9000)
